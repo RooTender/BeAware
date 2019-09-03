b498ea7d0bf3e865abb302fd907b497047869ed8
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2019-09-03 20:52:28 +0200

    Add project files.

---
+++ BeAware/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace BeAware
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notify
+            // 
+            this.notify.Icon = ((System.Drawing.Icon)(resources.GetObject("notify.Icon")));
+            this.notify.Text = "notify";
+            this.notify.Visible = true;
+            this.notify.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time limit (in minutes):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(240, 18);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "120";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(17, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Set default";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // save
+            // 
+            this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.save.Location = new System.Drawing.Point(177, 138);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(120, 35);
+            this.save.TabIndex = 4;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Time elapsed: 0 minutes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 185);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BeAware - Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notify;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BeAware/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BeAware
+{
+    public partial class Form1 : Form
+    {
+        private int _time = 0;
+        private int limit;
+
+        private void popup(double time)
+        {
+            limit = (int)Properties.Settings.Default["limit"];
+
+            if (time % 30 == 0)
+            {
+                notify.BalloonTipText = "Another 30 minutes passed 💨!\n" + time / 60 + " hours elapsed.";
+                notify.ShowBalloonTip(3);
+            }
+
+            if (time == limit - 5)
+            {
+                notify.BalloonTipText = "Only 5 minutes left 🙉!";
+                notify.ShowBalloonTip(3);
+            }
+
+            if (time == limit - 3)
+            {
+                notify.BalloonTipText = "Only 3 minutes left 🙊!";
+                notify.ShowBalloonTip(3);
+            }
+
+            if (time == limit - 1)
+            {
+                notify.BalloonTipText = "Only 1 MINUTE left 🙈!";
+                notify.ShowBalloonTip(3);
+            }
+
+            if (time >= limit)
+            {
+                notify.BalloonTipText = "Time's up 💥💤!";
+                notify.ShowBalloonTip(3);
+                Thread.Sleep(5000);
+                Application.SetSuspendState(PowerState.Suspend, true, true);
+                Application.Exit();
+            }
+        }
+
+        private void calcTime()
+        {
+            try
+            {
+                int max = int.Parse(textBox1.Text);
+
+                if (max < 60)
+                    label2.Text = max + " minutes";
+                else
+                    label2.Text = (max / 60) + " hours and " + (max % 60) + " minutes";
+            }
+            catch (Exception)
+            {
+                textBox1.Text = "0";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            notify.Visible = true;
+            calcTime();
+            timer1.Start();
+            textBox1.Text = Properties.Settings.Default["limit"].ToString();
+            notify.BalloonTipText = "BeAware is working ⏰!";
+            notify.ShowBalloonTip(1);
+            notify.BalloonTipText = "Double click the icon to show settings 🛠";
+            notify.ShowBalloonTip(1);
+            WindowState = FormWindowState.Minimized;
+            ShowInTaskbar = false;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+                notify.BalloonTipText = "Double click again to hide settings 😉";
+                notify.ShowBalloonTip(3);
+            }
+            else
+                WindowState = FormWindowState.Minimized;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _time++;
+            label3.Text = "Time elapsed: ";
+
+            if (_time < 60)
+                label3.Text += _time + " minutes";
+            else
+                label3.Text += (_time / 60) + " hours and " + (_time % 60) + " minutes";
+
+            popup(_time);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Properties.Settings.Default["limit"] = 120;
+            Properties.Settings.Default.Save();
+            timer1.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            calcTime();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+                e.Handled = true;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox1.Text) < 10)
+                MessageBox.Show("Time limit must be greater than 10 minutes!", "Time delay", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                timer1.Stop();
+                notify.BalloonTipText = "Saved ✔";
+                notify.ShowBalloonTip(2);
+                Properties.Settings.Default["limit"] = int.Parse(textBox1.Text);
+                Properties.Settings.Default.Save();
+                timer1.Start();
+            }
+        }
+    }
+}
